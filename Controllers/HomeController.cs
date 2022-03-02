fc11eef9648526809a2a9f0380b2c13d8c2ebcab
Eyenessus <eyenessus@gmail.com>
2022-03-02 17:05:39 -0300

    Aplicação de logs e administrações

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hotsite.Models;
+
+namespace Hotsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogWarning("Log de informações capturada da Index");
+             //captura logs da index
+            return View();
+           
+        }
+
+        [HttpPost]
+        public IActionResult Cadastrar(Interesse cad)
+        {
+            DatabaseService dbs = new DatabaseService();
+            dbs.CadastraInteresse(cad);
+            return View("Index",cad);
+        }
+
+    }
+}
